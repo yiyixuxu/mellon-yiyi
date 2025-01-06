c745b4bfe9c67b38d38ae0d01e637537a6526a75
cubiq <matteo@elf.io>
2025-01-06 20:35:08 +0100

    update for recent mellon update

--- sdxl_layers.py
+++ sdxl_layers.py
@@ -1,4 +1,4 @@
-SDXL_LAYERS = {
+SDXL_LAYERS = [
     "down.blocks.1.attentions.0",
     "down.blocks.1.attentions.1",
     "down.blocks.2.attentions.0",
@@ -10,4 +10,4 @@ SDXL_LAYERS = {
     "up.blocks.1.attentions.0",
     "up.blocks.2.attentions.1",
     "up.blocks.3.attentions.2",
-}
+]
